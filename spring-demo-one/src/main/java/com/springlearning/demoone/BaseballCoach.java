c2e52b5f499adb3e029b071985bc43206acfc47e
srinivasuvaka2011 <srinivasuvaka10@gmail.com>
2019-07-26 01:34:08 +0530

    created simple spring project

---
+++ spring-demo-one/src/main/java/com/springlearning/demoone/BaseballCoach.java
@@ -0,0 +1,9 @@
+package com.springlearning.demoone;
+
+public class BaseballCoach implements Coach {
+
+	@Override
+	public String getDailyWorkout() {
+		return "Spend 30 mins on batting practice";
+	}
+}
